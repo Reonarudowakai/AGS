1e1259c847d544f733641814fe141ee0c480ac51
Leonardo Henrique <reyozatsuki@gmail.com>
2021-09-18 20:37:38 -0300

    Treinando os comandos Git

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+var nome = window.prompt("Qual seu nome disgraça?!")
+document.write(`O seu nome tem ${nome.length} letras`)
+window.alert(`É um prazer te conhecer ${nome}`)
+*/
+
+/*
+var n1 = Number(window.prompt("Digite um numero"))
+var n2 = Number(window.prompt("Digite outro numero"))
+var s = (n1 + n2)
+
+s = window.alert(`A soma entre ${n1} e ${n2} é igual à ${s}`)
+*/
+
+var p1 = window.document.getElementsByTagName('p')[1]
+
+var d = document.getElementById('msg')
+
+d.style.background = 'green'
+d.innerText = 'Estou aguardando'
+
+
+
+
+
+
